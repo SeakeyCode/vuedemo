73b9bd57bceb2f302aaa3265e7e5f7dea520de26
chenhaifei <15378702772@163.com>
2019-10-31 22:13:36 +0800

    设计模式之链式调用

--- src/router/index.js
+++ src/router/index.js
@@ -2,13 +2,15 @@ import Vue from 'vue'
 import Router from 'vue-router'
 
 import Home from '@/components/home.vue'
+import Chain from '@/components/designPattern/chain.vue'
 
 Vue.use(Router)
 
 export default new Router({
     routes: [
         { path: '/home', component: Home },
-        { path: '/', redirect: '/home' }
+        { path: '/chain', component: Chain },
+        { path: '/', redirect: '/chain' }
     ]
 }
 )
