b509d8d2e8447dcd30d4127ca21a8edf57e4240b
chenhaifei <649719596@qq.com>
2019-11-05 10:20:20 +0800

    feat: codemirror

--- src/router/index.js
+++ src/router/index.js
@@ -2,13 +2,15 @@ import Vue from 'vue'
 import Router from 'vue-router'
 
 import Home from '@/components/home.vue'
+import Markdown from '@/components/markdown.vue'
 
 Vue.use(Router)
 
 export default new Router({
     routes: [
         { path: '/home', component: Home },
-        { path: '/', redirect: '/home' }
+        { path: '/markdown', component: Markdown },
+        { path: '/', redirect: '/markdown' }
     ]
 }
 )
