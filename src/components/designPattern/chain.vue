ba55f148788a0464723e28e1b9369309e8f63b24
chenhaifei <15378702772@163.com>
2019-11-27 23:44:50 +0800

    feta: 拖拽动画

--- src/components/designPattern/chain.vue
+++ src/components/designPattern/chain.vue
@@ -2,13 +2,13 @@
   <div class="hello">
     <div id="example1" class="list-group col">
       <div draggable 
-      @dragstart="dragStart($event, index)" 
+      @dragstart="dragStart($event, item, index)" 
       @dragover="allowDrop" 
       @drop="drop($event, index)" 
-      :class="`list-group-item list-group-item-${index}`" 
+      class="list-group-item" 
+      :class="{'list-group-item1': index === endIndex}"
       v-for="(item, index) in list" :key="index">{{item}}</div>
     </div>
-    <div class="list-item">456465456465</div>
   </div>
 </template>
 
@@ -17,29 +17,34 @@ export default {
   name: 'transition-example-2',
   data() {
     return {
-      list: [1,2,3,4,5],
-      startIndex: ''
+      list: [
+        '1FLIP是一种记忆设备和技术，最早是由@Paul Lewis提出的，FLIP是First、Last、Invert和Play四个单词首字母的缩写。这里简单的陈述一下',
+        '2FLIP是一种记忆设备和技术，最早是由@Paul Lewis提出的，FLIP是First、Last、Invert和Play四个单词首字母的缩写。这里简单的陈述一下',
+        '3FLIP是一种记忆设备和技术，最早是由@Paul Lewis提出的，FLIP是First、Last、Invert和Play四个单词首字母的缩写。这里简单的陈述一下',
+        '4FLIP是一种记忆设备和技术，最早是由@Paul Lewis提出的，FLIP是First、Last、Invert和Play四个单词首字母的缩写。这里简单的陈述一下'
+      ],
+      startIndex: '',
+      startItem: '',
+      endIndex: ''
     };
   },
   methods: {
     allowDrop(e){
       e.preventDefault();
     },
-    dragStart (e, index) {
+    dragStart (e, item, index) {
      this.startIndex = index
+     this.startItem = item
+      this.endIndex = null
+
     },
     drop (e, index) {
       this.allowDrop(e);
-      let eleClass = document.querySelector(`.list-group-item-${this.startIndex}`)
-      let targetEleClass = document.querySelector(`.list-group-item-${index}`)
-      let targetEleClassItem = document.querySelector('.list-item')
-      eleClass.style.height = 0  + 'px'
-      window.requestAnimationFrame(() => {
-        targetEleClassItem.style.height = 44  + 'px'
-        targetEleClass.before(targetEleClassItem)
-      })
-      console.log(index)
-      console.dir(eleClass)
+      this.endIndex = index
+      let allList = this.list
+      allList.splice(this.startIndex, 1)
+      allList.splice(index, 0, this.startItem)
+      this.list = allList
     }
   },
   mounted() {
@@ -54,17 +59,28 @@ export default {
   .blue-background-class {
     background-color: #C8EBFB;
   }
+  .list-group-item1{
+    color: #fff;
+    background-color: green !important;
+    transform: height 0.3s;
+    animation: mymove 0.3s;
+  }
+  @keyframes mymove {
+    from {height: 0;}
+    to {height: 122px}
+  }
+
   .list-item {
-    height: 0;
     position: relative;
     /* display: block; */
     margin-bottom: -1px;
     background-color: #fff;
     overflow: hidden;
     border: 1px solid rgba(0,0,0,.125);
-    transition: height 1s ease-out;
   }
   .list-group {
+    justify-content: center;
+    align-items: center;
     display: -ms-flexbox;
     display: flex;
     -ms-flex-direction: column;
@@ -75,10 +91,16 @@ export default {
   .list-group-item{
     position: relative;
     display: block;
-    height: 44px;
+    height: 122px;
+    padding: 10px;
+    box-sizing: border-box;
+    font-size: 14px;
+    border-radius: 6px;
+    width: 245px;
     margin-bottom: -1px;
     background-color: #fff;
-    border: 1px solid rgba(0,0,0,.125);
+    border: 1px solid red;
     transition: height 1s ease-out;
+    margin-bottom: 10px
   }
 </style>
