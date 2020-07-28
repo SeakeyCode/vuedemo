19233822caaebab2dfcd653461006b38e4f3c298
陈海飞 <649719596@qq.com>
2020-07-28 19:02:05 +0800

    fix: update

--- src/router/index.js
+++ src/router/index.js
@@ -6,7 +6,7 @@ import About from '@/components/about'
 import domDiff from '@/components/domDiff'
 import editing from '@/components/editing'
 import minder from '@/components/minder'
-import textcom from '@/components/textcom'
+// import textcom from '@/components/textcom'
 
 Vue.use(Router)
 
@@ -21,7 +21,7 @@ export default new Router({
         { path: '/domDiff', component: domDiff },
         { path: '/editing', component: editing },
         { path: '/minder', component: minder },
-        { path: '/textcom', component: textcom },
+        // { path: '/textcom', component: textcom },
         { path: '*', component: About },
       ],
     },
